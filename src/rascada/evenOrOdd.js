150960d94e8d6911e6d10c3f6dbedb9e5324ccfd
rascada <carftjr@gmail.com>
2016-01-18 21:23:59 +0100

    even or odd from lesson

--- src/rascada/evenOrOdd.js
+++ src/rascada/evenOrOdd.js
@@ -5,5 +5,28 @@
   */
 
 let evenOrOdd = n => n % 2 == 0 ? 'even' : 'odd';
+let number = +prompt('Provide number');
 
-console.log( evenOrOdd(+prompt('Provide number')) );
+console.log(evenOrOdd(question));
+
+/**
+  * even or odd written on lesson, without console.log and minimum es6
+  * @param {Number} n.
+  */
+
+function EVEN_OR_ODD(n) {
+  //document.write(n % 2 == 0 ? 'even' : 'odd');
+  document.body.innerText = n % 2 == 0 ? 'even' : 'odd';
+}
+
+EVEN_OR_ODD(+prompt('Provide number'));
+
+/**
+  * one line code
+
+  * good
+  document.innerText = +prompt('Podaj liczbę') % 2 == 0 ? 'parzysta' : 'nie parzysta';
+
+  * bad
+  document.write( +prompt('Podaj liczbę') % 2 == 0 ? 'parzysta' : 'nie parzysta' );
+  */
