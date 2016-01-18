fb7195b1c1c333c09391756e13f57a646237b2ce
rascada <carftjr@gmail.com>
2016-01-18 21:22:27 +0100

    add jsdoc to evenOrOdd

--- src/rascada/evenOrOdd.js
+++ src/rascada/evenOrOdd.js
@@ -1,3 +1,9 @@
+/**
+  * even or odd - normal code
+  * @param {Number} n.
+  * @return {String}
+  */
+
 let evenOrOdd = n => n % 2 == 0 ? 'even' : 'odd';
 
 console.log( evenOrOdd(+prompt('Provide number')) );
