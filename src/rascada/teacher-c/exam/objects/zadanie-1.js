de97a78c92fe7c07561af68f046e8a88e996c8f8
Damian <rascada@users.noreply.github.com>
2016-04-07 08:35:51 +0200

    Update zadanie-1.js

--- src/rascada/teacher-c/exam/objects/zadanie-1.js
+++ src/rascada/teacher-c/exam/objects/zadanie-1.js
@@ -1,24 +1,24 @@
 'use strict';
 
-		class Student {
-			constructor(imie, nazwisko, wiek, stypendium, rok_studiow) {
-				this.imie = imie;
-				this.wiek = wiek;
-				this.nazwisko = nazwisko;
-				this.stypendium = stypendium;
-				this.rok_studiow = rok_studiow;
+class Student {
+	constructor(imie, nazwisko, wiek, stypendium, rok_studiow) {
+		this.imie = imie;
+		this.wiek = wiek;
+		this.nazwisko = nazwisko;
+		this.stypendium = stypendium;
+		this.rok_studiow = rok_studiow;
 
-				this.show();
-			}
+		this.show();
+	}
 
-			show() {
-				const keys = Object.keys(this);
+	show() {
+		const keys = Object.keys(this);
 
-				keys
-					.map(key => key.replace('_', ' '))
-					.forEach(key =>
-						document.body.innerText += `${key}: ${this[key] || 'nie podano'} \n`);
-			}
-		}
+		keys
+			.map(key => key.replace('_', ' '))
+			.forEach(key =>
+				document.body.innerText += `${key}: ${this[key] || 'nie podano'} \n`);
+	}
+}
 
-		const student = new Student('Damian', 'Martyniak');
+const student = new Student('Damian', 'Martyniak');
