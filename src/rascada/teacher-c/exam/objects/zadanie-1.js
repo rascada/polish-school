2d8155e87a8afff017492e0da4c60efe3e8c7137
Damian <rascada@users.noreply.github.com>
2016-04-07 08:24:56 +0200

    Create zadanie-1.js

---
+++ src/rascada/teacher-c/exam/objects/zadanie-1.js
@@ -0,0 +1,24 @@
+'use strict';
+
+		class Student {
+			constructor(imie, nazwisko, wiek, stypendium, rok_studiow) {
+				this.imie = imie;
+				this.wiek = wiek;
+				this.nazwisko = nazwisko;
+				this.stypendium = stypendium;
+				this.rok_studiow = rok_studiow;
+
+				this.show();
+			}
+
+			show() {
+				const keys = Object.keys(this);
+
+				keys
+					.map(key => key.replace('_', ' '))
+					.forEach(key =>
+						document.body.innerText += `${key}: ${this[key] || 'nie podano'} \n`);
+			}
+		}
+
+		const student = new Student('Damian', 'Martyniak');
