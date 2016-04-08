a0ddcc487ff54c1313a18a1b2e8b0fef34e57fb3
Damian <rascada@users.noreply.github.com>
2016-04-08 15:57:01 +0200

    Update zadanie-4.js

--- src/rascada/teacher-c/exam/objects/zadanie-4.js
+++ src/rascada/teacher-c/exam/objects/zadanie-4.js
@@ -1,13 +1,10 @@
 'use strict';
 
-const card = ['Damian Martyniak', 'Programista', 'damian@syntax-shell.me'];
-
-card
+['Damian Martyniak', 'Programista', 'damian@syntax-shell.me']
 	.map((text, i) =>
 		switch (i) {
 			case 0: return text.bold();
 			case 1: return text.strike();
 			case 2: return text.small();
 		}
-	)
-	.forEach(text => document.body.innerHTML += `${text} <br>`);
+	).forEach(text => document.body.innerHTML += `${text} <br>`);
